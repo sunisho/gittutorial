88f21e04d1c92fef7176761c1d73be475648ab56
Sunish Oturkar <sunish.oturkar (at) gmail.com>
2016-12-12 19:04:50 -0500

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Sunish Oturkar (sunish.oturkar@gmail.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
